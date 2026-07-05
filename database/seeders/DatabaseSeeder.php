963742ac47369c7c0736c80c01bd0abafeb9ecf3
Ahmadnus <ahmad.mog2091@gmail.com>
2026-07-05 21:48:20 +0300

    Wire up seeders and update .env.example DB settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,15 +6,13 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seeder calls are appended here as each module is completed.
-     * Module 2 (Roles & Permissions) will add:
-     *   - RolePermissionSeeder (Super Admin, Admin, Manager, Cashier,
-     *     Kitchen, Delivery roles + full permission matrix)
-     *   - AdminUserSeeder (default super-admin login)
-     */
     public function run(): void
     {
-        //
+        $this->call([
+            RolePermissionSeeder::class,
+            AdminUserSeeder::class,
+            CategoryMenuSeeder::class,
+            DeliveryAreaSeeder::class,
+        ]);
     }
 }
